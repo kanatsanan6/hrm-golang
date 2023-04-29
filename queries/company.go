7ae1099ec8387dc094839f541e84322ced522fb9
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-04-29 19:19:36 +0700

    feat: sort user by created_at

--- queries/company.go
+++ queries/company.go
@@ -1,6 +1,9 @@
 package queries
 
-import "github.com/kanatsanan6/hrm/model"
+import (
+	"github.com/kanatsanan6/hrm/model"
+	"gorm.io/gorm"
+)
 
 type CreateCompanyArgs struct {
 	Name string
@@ -17,7 +20,9 @@ func (q *SQLQueries) CreateCompany(args CreateCompanyArgs) (model.Company, error
 
 func (q *SQLQueries) FindCompanyByID(id uint) (model.Company, error) {
 	var company model.Company
-	if err := q.DB.Where("ID = ?", id).Preload("Users").First(&company).Error; err != nil {
+	if err := q.DB.Where("ID = ?", id).Preload("Users", func(db *gorm.DB) *gorm.DB {
+		return db.Order("created_at ASC")
+	}).First(&company).Error; err != nil {
 		return model.Company{}, err
 	}
 	return company, nil
