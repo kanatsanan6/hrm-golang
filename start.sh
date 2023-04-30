e7e32dc5952529080f89449feadbdad5235b05dd
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-04-30 15:54:00 +0700

    ci: source .env

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-echo "postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable"
+source .env
 /app/migrate -path /app/migration -database "postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable" -verbose up
 
 echo "start the app"
