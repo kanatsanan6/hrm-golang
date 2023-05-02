f55d8f608556c80bb21dd5d943208a11af6a7d1b
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-05-02 16:35:07 +0700

    chore: remove debug log

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "run db migration"
 source .env
-echo "postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable"
 /app/migrate -path /app/migration -database "postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable" -verbose up
 
 echo "start the app"
